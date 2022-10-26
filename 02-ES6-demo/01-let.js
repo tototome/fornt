e6f18eeda0453aa4a663b231a42b9af4792f953f
pp <sadas@AA.com>
2022-10-26 11:14:16 +0800

    ppp

--- 02-ES6-demo/01-let.js
+++ 02-ES6-demo/01-let.js
@@ -1,16 +1,16 @@
-let a
-let b,c,d
-let e 
-let arr=[1,2,3,4,5]
+let a;
+let b, c, d;
+let e;
+let arr = [1, 2, 3, 4, 5];
 //变量也是有作用域
 
 {
-    let f = '5'
+  let f = "5";
 }
 //  f is not defined
 // console.log(f)
-arr.forEach((i,index)=>{
-    console.log(i)
-})
+arr.forEach((i, index) => {
+  console.log(i);
+});
 
-console.log(arr)
+console.log(arr);
